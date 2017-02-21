1fa3248ec065ae55c43448eb659118c8781e2b24
Catherine Oxley <catherine.oxley@live.co.uk>
2017-02-21 12:16:15 +0000

    added array of animals to Species

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,6 +3,5 @@ class Animal < ApplicationRecord
   belongs_to :species
   has_many :bookings
   validates :name, presence: true
-  validates :species, inclusion: { in: ["Spider", "Snake", "Elephant", "T-Rex", "Unicorn", "Centaur", "Great-White Shark", "Achilles son of God"]}
   has_attachments :photos, maximum: 4
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221115135) do
+ActiveRecord::Schema.define(version: 20170221115354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170221115135) do
     t.string   "bio"
     t.integer  "price"
     t.string   "species"
+    t.integer  "species_id"
+    t.index ["species_id"], name: "index_animals_on_species_id", using: :btree
     t.index ["user_id"], name: "index_animals_on_user_id", using: :btree
   end
 
@@ -77,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170221115135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "animals", "species"
   add_foreign_key "animals", "users"
   add_foreign_key "bookings", "animals"
   add_foreign_key "bookings", "users"
