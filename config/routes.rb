9214fbfd545a29bb4792ead63f335a2b63eb36a6
Archie Gilbert <archiegilbert@me.com>
2017-02-21 16:39:32 +0000

    users can only edit their own profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "/profile/:id/edit", to: "profiles#edit"
-  patch "/profile/:id/edit", to: "profiles#update", as: "profile_edit"
+  get "my_profile/edit", to: "profiles#edit"
+  patch "my_profile/edit", to: "profiles#update", as: "profile_edit"
+
   get "/profile/:id/show", to: "profiles#show", as: "profile_show"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show, :destroy]
+  get '/my_profile', to: 'profiles#my_profile', as: "my_profile"
 
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
