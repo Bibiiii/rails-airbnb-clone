57a5a563e2abc480b9bff7b5056a6fa7908e0776
Josh Oppenheim <joppenheim1@gmail.com>
2017-02-22 14:33:30 +0000

    added maps to search

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -8,6 +8,12 @@ class AnimalsController < ApplicationController
       species = Species.find(params[:species])
       @animals = Animal.where(species: species)
     end
+
+      @hash = Gmaps4rails.build_markers(@flats) do |flat, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 
   def new
